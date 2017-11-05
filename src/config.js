010524440a1331be3f4906f13c1761c63322b8eb
Fabian Dietenberger <piano.fabian@t-online.de>
2017-11-05 14:42:06 +0100

    Typo

--- src/config.js
+++ src/config.js
@@ -7,7 +7,6 @@ const SESSION_ATTRIBUTES = {
     NAME: 'name'
 };
 
-
 const IMAGE_URLS = {
     WELCOME: {
         smallImageUrl: `https://s3-eu-west-1.amazonaws.com/${ INFRASTRUCTURE.fileStorage }/mountains.jpg`,
